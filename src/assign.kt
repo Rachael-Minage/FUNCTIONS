a3e082eb7742cb144fe49b8791ab1745764b0df3
Rachael-Minage <lauryneminagge@gmail.com>
2022-02-18 22:14:34 +0300

    first commit

---
+++ src/assign.kt
@@ -0,0 +1,23 @@
+fun main(){
+    salut("rachael")
+    modulus(245,8)
+    add(34,876,897,346)
+    interests("iloveswims")
+
+}
+fun salut(name: String){
+    println("hello" + name)
+}
+fun modulus(a:Int,b:Int){
+var modulus = a%b
+    println(modulus)
+}
+fun add(a:Int,b:Int,c:Int,d:Int){
+    var add = a+b+c+d
+    println(add)
+}
+fun interests(name: String){
+    var interests = ("iloveswims")
+    println(interests)
+
+}
